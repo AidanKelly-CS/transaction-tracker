04520d8cb14b52ebc11faf1f3e52cd4a72dc1874
Aidan Kelly CSM2018 <a.kelly.2018@uni.strath.ac.uk>
2021-09-05 20:57:00 +0100

    refactor styles

--- src/category/Category.js
+++ src/category/Category.js
@@ -3,16 +3,18 @@ import './Category.css';
 
 export default function Category({icon, color, type, onClick}) {
     
+    const highlighted = false;
+
     function createCategory(type){
         if(type === "sub"){
             return (
-                <div className="sub-category">
+                <div className="category sub-category">
                     <FontAwesomeIcon icon={icon} color={color} size="1x" onClick={onClick}/>
                 </div>
             );
         }else{
             return (
-                <div className="category">
+                <div className="category main-category">
                     <FontAwesomeIcon icon={icon} color={color} size="2x" onClick={onClick}/>
                 </div>
             );
