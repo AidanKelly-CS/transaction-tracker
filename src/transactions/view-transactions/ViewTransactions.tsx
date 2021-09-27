51da63557222295c2c92834fb423c47fca94ef51
Aidan Kelly <aidankelly.cs@outlook.com>
2021-09-27 21:20:24 +0100

    add type

--- src/transactions/view-transactions/ViewTransactions.tsx
+++ src/transactions/view-transactions/ViewTransactions.tsx
@@ -3,14 +3,14 @@ import { faHotdog } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useState } from 'react';
 import Transaction from './Transaction';
 import './Transaction.css';
+import { TransactionInterface } from './TransactionInterface';
 
 
 export default function ViewTransactions() {
     let [transactions, setTransactions] = useState([]);
 
-
     useEffect(() => {
-      const trans = JSON.parse(localStorage.getItem("transactions"));
+      const trans = JSON.parse(localStorage.getItem("transactions")) as TransactionInterface[];
       if(trans){
         setTransactions(trans);
       }
