2b89c9963c616253d0409e92b16852002c99f4e4
Aidan Kelly <aidankelly.cs@outlook.com>
2021-12-17 21:37:36 +0000

    format money in transactions

--- src/transactions/view-transactions/Transaction.tsx
+++ src/transactions/view-transactions/Transaction.tsx
@@ -8,7 +8,7 @@ export default function Transaction({category, total, icon}:TransactionInterface
         <div className="transaction container">
             <Category icon={icon} color={"orange"} main={true} selected={false} onClick={()=>{}}/>
             <p>{category}</p>
-            <p className="trans-total">{total}</p>
+            <p className="trans-total">£{parseFloat(total).toFixed(2)}</p>
         </div>
     );
 }
